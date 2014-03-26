2de8ccf3ff0994ea8fddc53974555edb0c4e189f
Jez Hill <jezhill@gmail.com>
2014-03-26 19:03:37 -0400

    batch file sensitive to whether EPOCS_PYTHON variable is set or not (could use global python installation for example instead of FullMonty, but note that 64-bit python will likely still have a problem unless we can build a 64-but BCI2000RemoteLib.dll)

--- app/gui/epocs.bat
+++ app/gui/epocs.bat
@@ -1,3 +1,5 @@
+@if "%EPOCS_PYTHON%"=="" goto skipconfig
 @set PYTHONHOME=%EPOCS_PYTHON%
 @set PATH=%PYTHONHOME%;%PATH%
+:skipconfig
 @start pythonw epocs.py --log=../../system-logs/epocs-log-###.txt
