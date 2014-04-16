d79f743c193011c081d04e46eca4a262e341fb15
Jez Hill <jezhill@gmail.com>
2014-04-15 21:12:10 -0400

    Added a whole lot of tickets and expanded the command-line-tool-corruption story
    Added command-line tools: the basics finally seem to work, but the specialized ones
    (BackgroundTriggerFilter, TrapFilter, RangeIntegrator) all fail with, for example:
    
    
    Configuration Error: TrapFilter::Preflight: Unexpected measurement unit in expression "500msec"
    Configuration Error: TrapFilter::Preflight: Unexpected measurement unit in expression "100ms"
    Configuration Error: FilterWrapper: : Error reading VisSignal message.
    Runtime Error: TrapFilter::Initialize: Unexpected measurement unit in expression "500msec"
    Runtime Error: TrapFilter::Initialize: Unexpected measurement unit in expression "100ms"
    Runtime Error: FilterWrapper: : Error reading VisSignal message.
    Runtime Error: FilterWrapper: Unhandled Win32 exception 0xc0000005: ACCESS_VIOLATION
    Aborting TrapFilter.
    Error writing to standard output

---
+++ app/gettools.bat
@@ -0,0 +1,9 @@
+@set SRC=C:\bci2000-svn\last-known-good\tools\cmdline
+copy %SRC%\bci_dat2stream.exe           tools\cmdline\
+copy %SRC%\bci_stream2mat.exe           tools\cmdline\
+copy %SRC%\TransmissionFilter.exe       tools\cmdline\
+copy %SRC%\IIRBandpass.exe              tools\cmdline\
+copy %SRC%\BackgroundTriggerFilter.exe  tools\cmdline\
+copy %SRC%\TrapFilter.exe               tools\cmdline\
+copy %SRC%\RangeIntegrator.exe          tools\cmdline\
+@pause
