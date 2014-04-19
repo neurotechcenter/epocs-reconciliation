e1a280eda092e7b2dd0bb44c75e6902186017494
Jez Hill <jezhill@gmail.com>
2014-04-18 23:21:10 -0400

    Removed tickets. Added scraps one level higher up, including equipment list. Updated icon, added svg source.

---
+++ scraps/parent.py
@@ -0,0 +1,20 @@
+# VS2010 reg key 6VPJ7-H3CXH-HBTPT-X4T74-3YVY7
+
+self.parent.after()
+self.parent.after_cancel()
+
+self.parent.fs
+self.parent.sbs
+self.parent.lookback
+self.parent.modenames[ self.mode ]
+self.parent.axiscontrollers_emg1[ -1 ]
+
+self.parent.Log()
+self.parent.SetBarLimits()
+self.parent.SetTarget()
+
+self.parent.operator.params
+
+self.parent.operator.Set()
+self.parent.operator.GetVolts()
+self.parent.operator.LastRunNumber()
