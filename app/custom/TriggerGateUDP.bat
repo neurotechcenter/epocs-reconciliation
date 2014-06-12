e0f24655cf6e4f5b407c716e4f59b9b0c3643f2e
Jez Hill <jezhill@gmail.com>
2014-06-12 13:26:06 -0400

    removed extraneous misleading comments

--- app/custom/TriggerGateUDP.bat
+++ app/custom/TriggerGateUDP.bat
@@ -3,8 +3,3 @@ SET PARAMETER TriggerExpression RequestStimOn
 
 SET PARAMETER     Connector:ConnectorInput list   ConnectorInputFilter=  1 RequestStimOn    // list of state names or signal elements to allow, "*" for any, signal elements as in "Signal(1,0)"
 SET PARAMETER     Connector:ConnectorInput string ConnectorInputAddress=   localhost:20320  // IP address/port to read from, e.g. localhost:20320
-
-
-# SetConfig
-#    SendConditioningParameters
-#    GetSignalParameters
