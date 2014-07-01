f6ebc97240bff2e7bd04c6ee6d44521cd3c1d322
Jez Hill <jezhill@gmail.com>
2014-07-01 12:41:52 -0400

    added helper script for setting env variable

---
+++ scraps/set_pythonhome.bat
@@ -0,0 +1,24 @@
+:: On Windows 7+ you will need to "Run as administrator" rather than just run
+:: this file.
+
+:: Note that the changes to environment variables won't be initially recognized. You
+:: need to restart, or go to Control Panel->System->Advanced->Environment variables
+:: and click OK, or use some binary that "broadcasts a window message", as explained
+:: in http://stackoverflow.com/questions/24500881 and
+::    http://stackoverflow.com/questions/20653028
+
+@set "PYTHONHOME_EPOCS=C:\FullMonty275\App"
+:: TODO: could make this more sensitive
+
+@set /p ANS=Path to parent directory of pythonw.exe (default is %PYTHONHOME_EPOCS%): 
+@if "%ANS%" == "" goto skipreplace
+@set "PYTHONHOME_EPOCS=%ANS%"
+:skipreplace
+
+@set "EnvKey=HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
+@echo Adding PYTHONHOME_EPOCS=%PYTHONHOME_EPOCS% to system environment variables:
+@reg add "%EnvKey%" /v PYTHONHOME_EPOCS /t REG_SZ /d "%PYTHONHOME_EPOCS%
+
+
+@pause
+@SystemPropertiesAdvanced
